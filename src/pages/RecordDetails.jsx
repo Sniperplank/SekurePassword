248767da8dc073919bb8d210d5601eb1ff3c3a6c
Eilaf Aljundi <elafjundi@gmail.com>
2024-12-24 22:11:59 -0500

    different pages added

---
+++ src/pages/RecordDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RecordDetails() {
+  return (
+    <div>RecordDetails</div>
+  )
+}
+
+export default RecordDetails
---
+++ src/pages/RecordsList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RecordsList() {
+  return (
+    <div>RecordsList</div>
+  )
+}
+
+export default RecordsList
