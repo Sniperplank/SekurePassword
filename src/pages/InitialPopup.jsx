382ce24741c037ea95d8dc02e89e5ebfbe70a637
Eilaf Aljundi <elafjundi@gmail.com>
2024-12-25 21:56:24 -0500

    records list api call based on user logged in

--- src/pages/InitialPopup.jsx
+++ src/pages/InitialPopup.jsx
@@ -1,4 +1,4 @@
-import { Box, Stack, Typography, Button, Input } from '@mui/material'
+import { Box, Stack, Typography, Button, Input, OutlinedInput } from '@mui/material'
 import axios from 'axios'
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
@@ -18,9 +18,8 @@ function InitialPopup() {
     const signup = async (formData, setError) => {
         try {
             const data = await axios.post('http://localhost:5000/user/signup', formData)
-            // console.log(JSON.stringify({ ...data }))
             console.log(data)
-            navigate('/list')
+            navigate('/list', { state: { user: data.data } })
         } catch (error) {
             setError(error.response.data.message)
             console.log(error)
@@ -30,9 +29,8 @@ function InitialPopup() {
     const signin = async (formData, setError) => {
         try {
             const data = await axios.post('http://localhost:5000/user/signin', formData)
-            // console.log(JSON.stringify({ ...data }))
             console.log(data)
-            navigate('/list')
+            navigate('/list', { state: { user: data.data } })
         } catch (error) {
             setError(error.response.data.message)
             console.log(error)
@@ -61,14 +59,14 @@ function InitialPopup() {
                         {
                             isSignup && (
                                 <Stack direction='row' spacing={2}>
-                                    <Input variant='outlined' name='firstName' label='First Name' onChange={handleChange} width='50%' autoFocus />
-                                    <Input variant='outlined' name='lastName' label='Last Name' onChange={handleChange} width='50%' />
+                                    <OutlinedInput variant='outlined' name='firstName' label='First Name' onChange={handleChange} width='50%' autoFocus />
+                                    <OutlinedInput variant='outlined' name='lastName' label='Last Name' onChange={handleChange} width='50%' />
                                 </Stack>
                             )
                         }
-                        <Input variant='outlined' name='email' label='Email' onChange={handleChange} type='email' />
-                        <Input variant='outlined' name='password' label='Password' onChange={handleChange} type='password' />
-                        {isSignup && <Input variant='outlined' name='confirmPassword' label='Confirm Password' onChange={handleChange} type='password' />}
+                        <OutlinedInput variant='outlined' name='email' label='Email' onChange={handleChange} type='email' />
+                        <OutlinedInput variant='outlined' name='password' label='Password' onChange={handleChange} type='password' />
+                        {isSignup && <OutlinedInput variant='outlined' name='confirmPassword' label='Confirm Password' onChange={handleChange} type='password' />}
                         <Typography variant='h6' color='error'>{error}</Typography>
                         <Button type='submit' fullWidth variant='contained' color='primary'>
                             {isSignup ? 'Sign Up' : 'Sign In'}
--- src/pages/RecordsList.jsx
+++ src/pages/RecordsList.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import { Box, Button, CircularProgress, Stack, Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import AddIcon from '@mui/icons-material/Add';
+import axios from 'axios';
+import { useLocation } from 'react-router-dom';
 
 function RecordsList() {
+  const [records, setRecords] = useState({})
+  const [update, setUpdate] = useState(0)
+  const location = useLocation()
+  const user = location.state?.user
+
+  const updatePage = () => {
+    setUpdate(prev => prev + 1)
+  }
+
+  useEffect(() => {
+    async function getRecords() {
+      console.log(user)
+      const records = await axios.get('http://localhost:5000/record?email=' + user.result.email)
+      setRecords(records.data)
+    }
+    getRecords()
+  }, [])
+
   return (
-    <div>RecordsList</div>
+    <Box p={{ xs: 1, sm: 10 }} pl={{ xs: 1, sm: 20 }} pr={{ xs: 1, sm: 20 }}>
+      <Typography variant='h4' color='primary' textAlign='center'>Password Records</Typography>
+      <Button variant='contained' color='primary' startIcon={<AddIcon />} sx={{ height: 40, textTransform: 'none', float: 'right', marginRight: 7 }}>New Entry</Button>
+      {
+        !records.length ?
+          <CircularProgress size={50} />
+          : (
+            <>
+              {
+                Object.entries(records).map(([key, value]) => {
+                  return (
+                    <Stack key={key} width='100%' spacing={6} sx={{ marginTop: 10 }}>
+                      <Typography variant='h4' color='primary' textAlign='center'>{value.title}</Typography>
+                    </Stack>
+                  )
+                })
+              }
+            </>
+          )
+      }
+    </Box>
   )
 }
 
