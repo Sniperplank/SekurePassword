f7c676432cfe2feff2d4a6f797c7c3c0874008c1
Eilaf Aljundi <elafjundi@gmail.com>
2024-12-25 23:51:42 -0500

    buttons handlers

--- src/pages/RecordsList.jsx
+++ src/pages/RecordsList.jsx
@@ -2,16 +2,25 @@ import { Box, Button, CircularProgress, Stack, Typography } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import AddIcon from '@mui/icons-material/Add';
 import axios from 'axios';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 function RecordsList() {
   const [records, setRecords] = useState({})
   const [update, setUpdate] = useState(0)
   const location = useLocation()
   const user = location.state?.user
+  const navigate = useNavigate()
 
   const updatePage = () => {
     setUpdate(prev => prev + 1)
+  }
+
+  const handleAddRecord = () => {
+    navigate('/add')
+  }
+
+  const handleRecordDetails = (record) => {
+    navigate('/details', { state: { record: record } })
   }
 
   useEffect(() => {
@@ -26,7 +35,7 @@ function RecordsList() {
   return (
     <Box p={{ xs: 1, sm: 10 }} pl={{ xs: 1, sm: 20 }} pr={{ xs: 1, sm: 20 }}>
       <Typography variant='h4' color='primary' textAlign='center'>Password Records</Typography>
-      <Button variant='contained' color='primary' startIcon={<AddIcon />} sx={{ height: 40, textTransform: 'none', float: 'right', marginRight: 7 }}>New Entry</Button>
+      <Button onClick={handleAddRecord} variant='contained' color='primary' startIcon={<AddIcon />} sx={{ height: 40, textTransform: 'none', float: 'right', marginRight: 7 }}>New Entry</Button>
       {
         !records.length ?
           <CircularProgress size={50} />
@@ -36,7 +45,7 @@ function RecordsList() {
                 Object.entries(records).map(([key, value]) => {
                   return (
                     <Stack key={key} width='100%' spacing={6} sx={{ marginTop: 10 }}>
-                      <Typography variant='h4' color='primary' textAlign='center'>{value.title}</Typography>
+                      <Button onClick={() => handleRecordDetails(value)}>{value.title}</Button>
                     </Stack>
                   )
                 })
